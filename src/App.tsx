86795d1c8c83a745fbefc4ddcee64eb8e2162df5
zo11o <372526745@qq.com>
2020-07-08 23:57:39 +0800

    feat: 首页改造

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import './App.css';
-import Layout from './components/layout'
+import Layout from './components/layout/index'
 
 function App() {
   return (
     <div className="App">
-      你好啊 欢迎来到这里
-
       <Layout></Layout>
     </div>
   );
--- src/components/layout/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const layout = () => {
- return <div>这里是头部</div>
-}
-
-export default layout
